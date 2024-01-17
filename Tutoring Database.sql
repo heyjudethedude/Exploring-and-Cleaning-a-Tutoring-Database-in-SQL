370523e59aa19db1c995f625e45fc51ea188007b
Jude Magbanua <74355424+heyjudethedude@users.noreply.github.com>
2024-01-18 00:09:24 +0800

    Update Tutoring Database.sql

--- Tutoring Database.sql
+++ Tutoring Database.sql
@@ -1,12 +1,100 @@
 /*
+Tutoring Database Data Exploration 
 
-Cleaning Data in SQL Queries
+Skills used: Where, Count, Distinct, Joins, Case,  CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
 */
 
-
 SELECT * 
 FROM lesson_history
+
+SELECT * 
+FROM student_profile
+
+ -- Select Data that we are going to be starting with
+
+SELECT Date, Students, Topics, Feedback
+FROM lesson_history
+WHERE Date is not null
+ORDER BY 1, 4 DESC
+
+-- Total Sessions - Shows how many sessions were conducted in total.
+
+SELECT Count (Feedback)
+FROM lesson_history
+WHERE Date is not null
+
+-- Total Students - Shows how many distinct students are there in total.
+
+SELECT Count(Distinct(Students))
+FROM lesson_history
+WHERE Date is not null
+
+-- Total Sent Feedback 
+
+SELECT Count(Feedback)
+FROM lesson_history
+WHERE Feedback = 'sent'
+
+-- Percentage of feedback sent per month - Shows the percentage of feedbacks sent out of total sessions per month.
+
+SELECT 
+(COUNT(CASE WHEN Feedback = 'sent' THEN 1 END) * 100.0) / COUNT(feedback) AS 'Sent Feedback Percentage'
+FROM lesson_history
+WHERE MONTH(Date) = 11 
+
+-- Percentage of pending feedback per month - Shows the percentage of feedbacks still pending out of total sessions per month.
+
+SELECT 
+(COUNT(CASE WHEN Feedback = 'pending' THEN 1 END) * 100.0) / COUNT(feedback) AS 'Pending Feedback Percentage'
+FROM lesson_history
+WHERE MONTH(Date) = 10
+
+-- Busiest day of the week - Shows the day of the week with the most number of sessions
+
+SELECT TOP (1)
+DATENAME(dw, DATE) as busiest_day, Count(Feedback) as total_sessions
+FROM lesson_history
+Group by  DATENAME(dw, DATE)
+Order by total_sessions DESC
+
+-- Frequency of students per curriculum - Shows the number students in a given curriculum, ie. UK, American, IB, SABIS
+
+SELECT COUNT(Curriculum)
+FROM student_profile
+WHERE Curriculum = 'UK'
+
+-- Percentage of feedback sent per month - Shows the percentage of feedbacks sent out of total sessions per month.
+
+
+
+-- Join Two Tables -- Join the table and combine them for data visualization
+
+SELECT his.Date, his.Students, his.Topics, his.Feedback, 
+prof.Year_level, prof.Curriculum, prof.study_plan
+FROM dbo.lesson_history as his
+Join dbo.student_profile as prof
+	On his.Students = prof.Name
+WHERE Date is not null
+ORDER BY 1, 4 DESC
+
+-- CTE
+
+WITH combinedtable as 
+(
+SELECT his.Date, his.Students, his.Topics, his.Feedback, 
+prof.Year_level, prof.Curriculum, prof.study_plan
+FROM dbo.lesson_history as his
+Join dbo.student_profile as prof
+	On his.Students = prof.Name
+--WHERE Date is not null
+--ORDER BY 1, 4 DESC
+)
+SELECT 
+(COUNT(CASE WHEN Curriculum = 'UK' THEN 1 END) * 100.0) / COUNT(Curriculum) AS 'Percentage of UK Students'
+FROM combinedtable
+
+
 
 
 
